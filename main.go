457284d06656e86ff67db946ec8085a904521b6e
Rasmus Bach <hoppedyt@Rasmuss-MBP.lan>
2019-06-29 04:30:27 +0200

    First commit

---
+++ main.go
@@ -0,0 +1,98 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Note: the example only works with the code within the same release/branch.
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	//
+	// Uncomment to load all auth plugins
+	// _ "k8s.io/client-go/plugin/pkg/client/auth
+	//
+	// Or uncomment to load specific auth plugins
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/azure"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/openstack"
+)
+
+func main() {
+	// creates the in-cluster config
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// kubeClient, err = kclient.New(config)
+	// podInterface := kubeClient.Pods(namespace)
+	// podList, err := podInterface.List(kapi.ListOptions{})
+
+	for {
+		pods, err := clientset.CoreV1().Pods("myproject").List(metav1.ListOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+		//fmt.Printf("This is the pod stats %s\n", pods.auth)
+		// fmt.Printf("This is the pod stats %s\n", pods.)
+
+		// Examples for error handling:
+		// - Use helper functions like e.g. errors.IsNotFound()
+		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
+
+		// pod, err := clientset.CoreV1().Pods("myproject").Get(pod.Name, metav1.GetOptions{})
+		// fmt.Println(pod.Status)
+
+		for _, podder := range pods.Items {
+			fmt.Println("NAME:")
+			fmt.Println(podder.Name)
+			fmt.Println("STATUS")
+			fmt.Println(podder.Status)
+			fmt.Println("SPEC")
+			fmt.Println(podder.Spec)
+			fmt.Println("LABELS")
+			fmt.Println(podder.Labels)
+			fmt.Println("VOLUMES")
+			fmt.Println(podder.Volume)
+			fmt.Println("GENERATEDNAME")
+			fmt.Println(podder.GeneratedName)
+		}
+
+		if errors.IsNotFound(err) {
+			fmt.Printf("Pod not found\n")
+		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+			fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
+		} else if err != nil {
+			panic(err.Error())
+		} else {
+			fmt.Printf("Found pod\n")
+		}
+
+		time.Sleep(10 * time.Second)
+	}
+}
